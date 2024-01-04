9b6bef3b455fb712421b848f4d837abefaed15e4
Brian Yip <brianyip@Brians-MacBook-Air.local>
2024-01-03 21:11:08 -0700

    dimissCookie, purchaseTicketData, cities

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,6 +11,26 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add('dismissCookie', () => {
+  cy.get('body > div.wrapper > div:nth-child(1) > div.header > div:nth-child(2) > div > div > div > div > div > a')
+    .contains('I accept the use of cookies').then(($btn) => {
+      if ($btn.length){
+        $btn.click()
+      } else {
+        cy.log("cookie notification already dismissed")
+      }
+    });
+})
+
+Cypress.Commands.add('enterPurchaseTicketData', (departCity, departDate, arriveCity, arriveDate) => {
+  cy.get('#searchTimetableForm > div > div:nth-child(1) > div:nth-child(3) > div > input')
+    .type(departCity)
+  cy.get('#arrival-date').type(arriveCity)
+
+  cy.get('#datepicker-first').type(departDate)
+
+  cy.get('#datepicker-second').type(arriveDate)
+})
 //
 //
 // -- This is a child command --
--- cypress/tests/buytickets.cy.js
+++ cypress/tests/buytickets.cy.js
@@ -1,9 +1,19 @@
+import buyTicketData from '../fixtures/buy_tickets-date_data.json'
+
 describe('Buy Tickets', () => {
   beforeEach(function(){
     cy.visit('passageiros/en/buy-tickets');
+    cy.dismissCookie();
   })
 
   it('Cancel Ticket Purchase Before Payment', () => {
-    #TODO
+    const today = new Date()
+    cy.enterPurchaseTicketData('Lagos', 3, 'Porto - Campanha', 5)
   })
+
+  // buyTicketData.forEach((reservation) => {
+  //   it('Buy Ticket departing ' + reservation.departDate + ' days from today and arriving ' + reservation.arriveDate + ' from today', () => {
+  //
+  //   })
+  // })
 })
